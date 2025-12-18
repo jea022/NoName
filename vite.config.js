7b97b705a6c90275d051c4b20ae0cdb02ee151f9
jea022 <aguinagajuanes2003@gmail.com>
2025-12-18 17:07:22 -0500

    Arquitectura

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,21 @@ export default defineConfig({
   },
   build: {
     outDir: '../../dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+      mangle: true,
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]'
+      }
+    }
   }
 })
